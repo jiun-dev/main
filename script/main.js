984560ec018cc6af1be09e58facc81f208fb3026
jiun-dev <58166091+jiun-dev@users.noreply.github.com>
2020-01-02 13:28:08 +0900

    Update main.js

--- script/main.js
+++ script/main.js
@@ -41,10 +41,20 @@ $(function() {
 
 /* fade in out */
 
-$(window).scroll(function(){
-    $(".top").css("opacity", 1 - $(window).scrollTop() / 250);
-  });
-
+$(document).ready(function() {
+    /* 1 */
+    $(window).scroll( function(){
+        /* 2 */
+        $('.introduce').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* 3 */
+            if( bottom_of_window > bottom_of_object/10 ){
+                $(this).animate({'opacity':'1'},800);
+            }
+        }); 
+    });
+});
 
 
 
